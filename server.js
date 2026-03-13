3b04f5912e160507e6ecd7c00ab703913ede4f9e
kimth84931016-gif <kimth84931016@gmail.com>
2026-03-13 10:13:14 +0900

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const STATUS_CSV_URL =
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vSkuiyWVse5fkRy2DOIe3umh2_PhkAlWthbYtP6AIxU8XGnMPl7vpFdaaMB3aucwGqe31FURworghkx/pub?gid=374063695&single=true&output=csv";
 
 const WRITE_URL =
-  "https://script.google.com/macros/s/AKfycbw-7Qh5QkmOYR17A6cH35pGxO4gL2P0Mo2iHfZrClzHiNgPz-nQmH8ukuf7MQy5Z1SP/exec";
+  "https://script.google.com/macros/s/AKfycbyKHS3G5ohU4iiVmII4q_YHtmy-UqrLwXucdxN-VsRFe3axvK0zF_PTUCuuR0NY0SoS/exec";
 
 /* -------------------- 공통 -------------------- */
 
