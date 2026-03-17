6074b10bc7bacf2a8eada17e4680d92963ccb3d6
kimth84931016-gif <kimth84931016@gmail.com>
2026-03-17 18:05:01 +0900

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const STATUS_CSV_URL =
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vSkuiyWVse5fkRy2DOIe3umh2_PhkAlWthbYtP6AIxU8XGnMPl7vpFdaaMB3aucwGqe31FURworghkx/pub?gid=374063695&single=true&output=csv";
 
 const WRITE_URL =
-  "https://script.google.com/macros/s/AKfycbzKVE2CBSyN98RVrysVi5BlD9p2G8TSJIhl1uLmCkaly5omHlkW9p_gaeeND8L2WNqG/exec";
+  "https://script.google.com/macros/s/AKfycbwOyXGKkarb_TV_6Sh1DNKg_CbcNvc7KEh14Km9pZKoTGkGnFztL0uabEoTpLf77qOT/exec";
 
 /* ---------------------------
  * 공통 유틸
